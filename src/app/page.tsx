3c1222bc8d0c7f65f36c20c3f2cd2fdacb3b016d
ryansoe <ryansoe26@gmail.com>
2026-02-27 15:31:52 -0800

    feat: projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,7 @@
-"use client";
+import { ProjectsView } from "@/features/projects/components/projects-view";
 
-import { useMutation, useQuery } from "convex/react";
-
-import { Button } from "@/components/ui/button";
-
-import { api } from "../../convex/_generated/api";
-
-const X = () => {
-  const projects = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
-
-  return (
-    <div className="flex flex-col gap-2 p-4">
-      <Button
-        onClick={() =>
-          createProject({
-            name: "New project123",
-          })
-        }
-      >
-        Add new
-      </Button>
-      {projects?.map((projects) => (
-        <div className="border rounded p-2 flex flex-col" key={projects._id}>
-          <p>{projects.name}</p>
-          <p>Owner Id: {projects.ownerId}</p>
-        </div>
-      ))}
-    </div>
-  );
+const Home = () => {
+  return <ProjectsView />;
 };
 
-export default X;
+export default Home;
