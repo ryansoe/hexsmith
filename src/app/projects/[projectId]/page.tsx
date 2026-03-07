5abbf94c057235604aab50dc2a38733ce362b5bb
ryansoe <ryansoe26@gmail.com>
2026-03-07 14:11:12 -0800

    fix: type erros

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -4,10 +4,10 @@ import { ProjectIdView } from "@/features/projects/components/project-id-view";
 const ProjectIdPage = async ({
   params,
 }: {
-  params: Promise<{ projectId: Id<"projects"> }>;
+  params: Promise<{ projectId: string }>;
 }) => {
   const { projectId } = await params;
-  return <ProjectIdView projectId={projectId} />;
+  return <ProjectIdView projectId={projectId as Id<"projects">} />;
 };
 
 export default ProjectIdPage;
